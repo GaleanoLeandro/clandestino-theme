a41c315bf4fcb853c24385c9fcc82ad0c61a0d12
Leandro Galeano <leandrogaleano96@hotmail.com>
2019-06-28 01:52:17 -0300

    Page 404 added

---
+++ template-parts/content-none.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="py-3 d-flex justify-content-center align-items-center">
+        <h1 class="m-5 text-w-3 text-1">
+            Content not found
+        </h1>
+    </div>
+</div>
