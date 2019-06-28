4ac4d6dec01ba73e08d994150b6d2db8b68a8f10
Leandro Galeano <leandrogaleano96@hotmail.com>
2019-06-28 01:10:49 -0300

    center title on single post

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -7,7 +7,7 @@
 </div>
 
 <div class="container">
-  <div class="py-3 d-flex justify-content-between align-items-center">
+  <div class="py-3 d-flex justify-content-center align-items-center">
     <h1 class="mb-0 text-w-3 text-1">
       <b><?php the_title(); ?></b>
     </h1>
