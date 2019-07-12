e4bb1a67bec09876587ca1a734b8a01b832fc565
Leandro Galeano <leandrogaleano96@hotmail.com>
2019-07-11 23:36:43 -0300

    build 1.0.6

--- components/card-home.php
+++ components/card-home.php
@@ -1,6 +1,6 @@
 <div class="card">
   <a class="text-body" href="<?php the_permalink() ?>">
-    <?php the_post_thumbnail('post-thumbnail') ?>
+    <?php the_post_thumbnail('medium_large') ?>
     <div class="card-with-img text-white d-flex align-items-center justify-content-center">
         <div class="card-with-img-content text-center">
           <h2 class="card-title m-0 text-w-1">
